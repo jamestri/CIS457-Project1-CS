f34a4955263704d1b9b4abd4a510359b14b6cbc0
jamestri <jamestri@mail.gvsu.edu>
2019-10-11 12:04:36 -0400

    Added functionality and ClientHandler class. List and retr are mostly complete as of right now

---
+++ src/ClientHandler.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+
+public class ClientHandler extends Thread{
+    Socket socket;
+
+    public ClientHandler(Socket connection) throws Exception{
+        this.socket = socket;
+    }
+
+
+    @Override
+    public void run() {
+        try{
+            processRequest();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+    private void processRequest() throws Exception{
+        DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String fromClient = inFromClient.readLine();
+
+        StringTokenizer tokens = new StringTokenizer(fromClient);
+        String frstln = tokens.nextToken();
+        int port = Integer.parseInt(frstln);
+        String clientCommand = tokens.nextToken();
+        if (clientCommand.equals("list:")) {
+
+            Socket dataSocket = new Socket(socket.getInetAddress(), port);
+            DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
+
+            //For when we need to use on a machine, set PATH to directory of server
+            File folder = new File("PATH");
+            String[] files = folder.list();
+
+            for (String file : files){
+                dataOutToClient.writeBytes(file);
+            }
+            dataOutToClient.writeBytes("\n");
+
+
+            dataSocket.close();
+            System.out.println("Data Socket closed");
+        }
+
+        //this might be in wrong place in code, dunno
+        if (clientCommand.startsWith("retr:")){
+            Socket dataSocket = new Socket(socket.getInetAddress(), port);
+            DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
+
+            String fileName = clientCommand.substring(6);//starting after the space
+
+            File folder = new File("PATH");
+            String[] files = folder.list();
+
+            File sendFile;
+            for (String file: files){
+                if (file.equals(fileName)){
+                    FileInputStream fis = new FileInputStream(fileName);
+                    //add headers and stuff and codes
+                    sendBytes(fis, dataOutToClient);
+                } else {
+                    dataOutToClient.writeBytes("550");
+                }
+            }
+
+            dataSocket.close();
+            System.out.println("Data Socket closed");
+        }
+
+        if (clientCommand.startsWith("stor:")){
+            Socket dataSocket = new Socket(socket.getInetAddress(), port);
+            DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
+
+            //TODO Fill in the rest of this and get file from this
+
+            dataSocket.close();
+            System.out.println("Data Socket closed");
+        }
+
+
+        if (clientCommand.equals("close")){
+            socket.close();
+            return;
+        }
+
+    }
+
+    private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception{
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+
+        while ((bytes = fis.read(buffer)) != -1){
+            os.write(buffer, 0, bytes);
+        }
+    }
+}
+
--- src/ftpserver.java
+++ src/ftpserver.java
@@ -14,56 +14,10 @@ class FTPServer {
 		int port;
 
 		while (true) {
+
+
 			Socket connectionSocket = welcomeSocket.accept();
-
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
-			fromClient = inFromClient.readLine();
-
-			StringTokenizer tokens = new StringTokenizer(fromClient);
-			frstln = tokens.nextToken();
-			port = Integer.parseInt(frstln);
-			clientCommand = tokens.nextToken();
-
-			if (clientCommand.equals("list:")) {
-
-				Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-				DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
-				//TODO Fill in the rest of this
-
-				dataSocket.close();
-				System.out.println("Data Socket closed");
-			}
-
-			//this might be in wrong place in code, dunno
-			if (clientCommand.equals("retr:")){
-				Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-				DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
-
-				//TODO Fill in the rest of this and get file from this
-
-				dataSocket.close();
-				System.out.println("Data Socket closed");
-			}
-
-			if (clientCommand.equals("stor:")){
-				Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-				DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
-
-				//TODO Fill in the rest of this and get file from this
-
-				dataSocket.close();
-				System.out.println("Data Socket closed");
-			}
-
-			//
-			if (clientCommand.equals("close")){
-				connectionSocket.close();
-				break;
-			}
-
+			ClientHandler handler = new ClientHandler(connectionSocket);
 		}
-
 	}
 }
